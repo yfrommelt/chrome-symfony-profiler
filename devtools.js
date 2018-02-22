beb05caac932cee54ca4fbc0016f6f9154ecdbb5
Yoann FROMMELT <yfrommelt@hevaweb.com>
2018-02-22 17:57:26 +0100

    add promotion images

--- devtools.js
+++ devtools.js
@@ -1,2 +1,2 @@
 // Create a tab in the devtools area
-chrome.devtools.panels.create("Symfony Profiler", "icon.png", "panel.html", function(panel) {});
+chrome.devtools.panels.create("Symfony Profiler", "icons/icon48.png", "panel.html", function(panel) {});
